61158f78baa511a30fe601a45c2c78e61e6d5c31
Joey Eremondi <joey@eremondi.com>
2014-08-13 13:49:39 -0600

    Added some documentation

--- src/DFA/DFAState.java
+++ src/DFA/DFAState.java
@@ -1,21 +1,39 @@
 package DFA;
 
 import java.util.*;
-import java.lang.Math;
-
-
-
 import automata.FAState;
-import automata.FiniteAutomaton;
 
 
 
-
+/**
+ * A module for representing Deterministic Finite Automata as sets of NFA states,
+ * lazily computing transitions and other states from a given NFA.
+ * 
+ * 
+ * 
+ * This means if you access a linear number of states, at most a quadratic amount of memory will be used
+ * since unaccessed states are never generated.
+ * Very useful for the shuffle decomposition algorithm, which only accesses a linear number of states.
+ *  
+ * @author Joey Eremondi
+ *
+ */
 public class DFAState {
 	
+	 /**
+	  * Set of NFA states represented by this DFA state
+	  */
 	 Set<FAState> superState;
+	 /**
+	  * The NFA whose determinization this DFA is
+	  */
 	 LabeledNFA owner;
 	
+	 /**
+	  * Generate a DFA state from a set of NFA states
+	  * @param subStates NFA states in this super-state
+	  * @param owner the NFA the states belong to
+	  */
 	protected DFAState(Set<FAState> subStates, LabeledNFA owner)
 	{
 		//System.err.println("Making super state " + subStates);
@@ -30,19 +48,20 @@ public class DFAState {
 		this.owner = owner;
 	}
 	
-	private static FAState first(Set<FAState> s){
-		for (FAState q : s)
-		{
-			return q;
-		}
-		throw new RuntimeException("No first element in empty list");
-	}
+
 	
+	/**
+	 * @return true if this is an empty super-state, that is, it represents failure in the NFA. False otherwise
+	 */
 	public boolean isSink()
 	{
 		return superState.isEmpty();
 	}
 	
+	/**
+	 * Test if this state is final in the DFA
+	 * @return true if this state contains a final NFA state
+	 */
 	public boolean isFinal()
 	{
 		for (FAState q : this.superState)
@@ -55,11 +74,65 @@ public class DFAState {
 		return false;
 	}
 	
+	/**
+	 * Accessor for the NFA this DFA is based off of
+	 * @return the NFA whose determinization this state is from
+	 */
 	public LabeledNFA owner()
 	{
 		return this.owner;
 	}
 	
+	/**
+	 * Get the set of all characters over which the DFA or a state has defined transitions
+	 * @return the DFA's alphabet
+	 */
+	public Set<Character> alphabet()
+	{
+		FAState firstState = first(this.superState);
+		Set<String> alphabetStr = firstState.getowner().getAllTransitionSymbols();
+		Set<Character> alphabet = new TreeSet<Character>();
+		for (String s : alphabetStr)
+		{
+			alphabet.add(s.charAt(0));
+		}
+		return alphabet;
+	}
+	
+	/**
+	 * Print a nice representation of a DFA state
+	 */
+	public String toString()
+	{
+		List<String> labels = new LinkedList<String>();
+		for (FAState sub : this.superState)
+		{
+			labels.add(this.owner.label(sub));
+		}
+		return labels.toString();
+	}
+	
+	//////// Static methods /////////////
+	
+	/**
+	 * Get an arbitrary element of a non-empty set of states
+	 * @param s the set of states to access
+	 * @return
+	 */
+	private static FAState first(Set<FAState> s){
+		for (FAState q : s)
+		{
+			return q;
+		}
+		throw new RuntimeException("No first element in empty list");
+	}
+	
+	/**
+	 * The one-step transition function for a DFA
+	 * @param q The "current" state
+	 * @param c An input character
+	 * @return The state reached after reading c from state q
+	 */
 	public static DFAState delta(DFAState q, char c)
 	{
 
@@ -81,6 +154,12 @@ public class DFAState {
 		return new DFAState(accum, q.owner());
 	}
 	
+	/**
+	 * The extended transition function for a DFA
+	 * @param q The "current" state
+	 * @param s Some string of input to be read
+	 * @return The state reached after reading all characters of s starting in state q
+	 */
 	public static DFAState dhat(DFAState q, StringBuilder s)
 	{
 		DFAState ret = q;
@@ -92,9 +171,14 @@ public class DFAState {
 		return ret;
 	}
 	
+	/**
+	 * Returns true if we can read the given word without failing
+	 * @param q the "current" state
+	 * @param s The word to be read
+	 * @return True if we can read s starting in state q without failing in the current DFA
+	 */
 	public static boolean dhatDefined(DFAState q, StringBuilder s)
 	{
-		boolean isDefined = true;
 		DFAState current = q;
 		for (int i = 0; i < s.length(); i++)
 		{
@@ -109,18 +193,13 @@ public class DFAState {
 		return true;
 	}
 	
-	public Set<Character> alphabet()
-	{
-		FAState firstState = first(this.superState);
-		Set<String> alphabetStr = firstState.getowner().getAllTransitionSymbols();
-		Set<Character> alphabet = new TreeSet<Character>();
-		for (String s : alphabetStr)
-		{
-			alphabet.add(s.charAt(0));
-		}
-		return alphabet;
-	}
+
 	
+	/**
+	 * 
+	 * @param q The "current" state
+	 * @return The set of all letters we can read from q without failing
+	 */
 	public static List<Character> deltaSet(DFAState q)
 	{
 		if (q.superState.isEmpty())
@@ -131,7 +210,7 @@ public class DFAState {
 		{
 			
 			FAState firstState = first(q.superState);
-			LabeledNFA aut = (LabeledNFA) firstState.getowner();
+			//LabeledNFA aut = (LabeledNFA) firstState.getowner();
 			//System.err.println("First state " + aut.label(firstState));
 			Set<String> alphabetStr = firstState.getowner().getAllTransitionSymbols();
 			Set<Character> alphabet = new TreeSet<Character>();
@@ -153,6 +232,10 @@ public class DFAState {
 		}
 	}
 	
+	/**
+	 * @param q The "current" state
+	 * @return an arbitrary member of deltaSet(q)
+	 */
 	public static char firstDeltaSet(DFAState q)
 	{
 		FAState firstState = first(q.superState);
@@ -164,7 +247,7 @@ public class DFAState {
 		}
 		
 		
-		LinkedList<Character> accum = new LinkedList<Character>();
+		//LinkedList<Character> accum = new LinkedList<Character>();
 		for (char a : alphabet)
 		{
 			if (deltaDefined(q,a))
@@ -175,6 +258,12 @@ public class DFAState {
 		return 0;
 	}
 	
+	/**
+	 * Returns true if we can read the given letter without failing
+	 * @param q the "current" state
+	 * @param c The word to be read
+	 * @return True if we can read c starting in state q without failing in the current DFA
+	 */
 	public static boolean deltaDefined(DFAState q,char c)
 	{
 		if (q.isSink())
@@ -203,7 +292,11 @@ public class DFAState {
 		return foundState;
 	}
 
-	
+	/**
+	 * Given an NFA with labeled states, return the start state of a DFA accepting the same language
+	 * @param aut The NFA to determinize
+	 * @return The determinization of aut
+	 */
 	public static DFAState initialDFAState(LabeledNFA aut)
 	{
 		//System.err.println("Making DFA with init state " + aut.getInitialState()); 
@@ -212,12 +305,21 @@ public class DFAState {
 		return new DFAState(init, aut);
 	}
 	
+	/**
+	 * Print all states and transitions of the determinization of an NFA
+	 * Runs forever on cyclic NFA's
+	 * @param aut An acyclic NFA to print
+	 */
 	public static void printAcyclic(LabeledNFA aut)
 	{
 		DFAState q = initialDFAState(aut);
 		printStateRecursive(q);
 	}
 	
+	/**
+	 * Print a state's information, then recursively print the information of all states it can access
+	 * @param q The first state whose information we print
+	 */
 	private static void printStateRecursive(DFAState q)
 	{
 		if (q.isSink())
@@ -239,14 +341,6 @@ public class DFAState {
 		}
 	}
 	
-	public String toString()
-	{
-		List<String> labels = new LinkedList<String>();
-		for (FAState sub : this.superState)
-		{
-			labels.add(this.owner.label(sub));
-		}
-		return labels.toString();
-	}
+
 
 }
--- src/DFA/LabeledNFA.java
+++ src/DFA/LabeledNFA.java
@@ -5,28 +5,49 @@ import java.util.*;
 import automata.FAState;
 import automata.FiniteAutomaton;
 
+/**
+ * An extension of the RABIT NFA class which requires each state to have a label
+ * Makes for nice pretty-printing of automata, and easier debugging
+ * 
+ * Also adds some functionality, such as finding the set of reachable states
+ * 
+ * @author Joey Eremondi
+ *
+ */
 public class LabeledNFA extends FiniteAutomaton
 {
+	//The mapping of states to labels
 	protected HashMap<FAState, String> labels = new  HashMap<FAState, String>();
+	//The set of all accessible states
 	protected HashSet<FAState> liveStates = new HashSet<FAState>();
-	
+
+	/**
+	 * Create a state and add its label to the NFA's label table
+	 * @param s The state's label
+	 * @return A new state of this NFA
+	 */
 	public FAState createState(String s) {
 		FAState ret = super.createState();
 		labels.put(ret, s);
 		return ret;
 	}
-	
+
 	@Override
 	public FAState createState()
 	{
 		throw new RuntimeException("Can't create state without label");
 	}
-	
+
+	/**
+	 * Return the label of a given state, just straightforward lookup
+	 * @param q The state to look up
+	 * @return The state's label
+	 */
 	public String label(FAState q)
 	{
 		return labels.get(q);
 	}
-	
+
 	@Override
 	public String toString() {
 		String result="\n";
@@ -47,67 +68,80 @@ public class LabeledNFA extends FiniteAutomaton
 		result+="\nACC:"+F+"\n";
 		return result;
 	}
-	
-		//Assumes NFA has no loops
-		//TODO deal with loops?
-		public void trim()
-		{
-			Set<FAState> reachable = new HashSet<FAState>();
-			dfs(this.getInitialState(), reachable);
-			
-			Set<FAState> canReachEnd = new HashSet<FAState>();
-			for (FAState f : this.F)
-			{
-				reverseDfs(f, canReachEnd);
-			}
-			
-			//Add all valid states to our final list
-			this.liveStates.clear();
-			for (FAState q : this.states)
-			{
-				if (reachable.contains(q) && canReachEnd.contains(q))
-				{
-					this.liveStates.add(q);
-				}
-			}
-			
 
-		}
-		
-		public boolean isLive(FAState q)
+
+	/**
+	 * Assuming this NFA is acyclic, calculate the set of all states which are reachable when reading some word.
+	 */
+	public void trim()
+	{
+		Set<FAState> reachable = new HashSet<FAState>();
+		dfs(this.getInitialState(), reachable);
+
+		Set<FAState> canReachEnd = new HashSet<FAState>();
+		for (FAState f : this.F)
 		{
-			return this.liveStates.contains(q);
+			reverseDfs(f, canReachEnd);
 		}
-		
-		private void dfs(FAState q, Set<FAState> seen)
+
+		//Add all valid states to our final list
+		this.liveStates.clear();
+		for (FAState q : this.states)
 		{
-			seen.add(q);
-			for (FAState next : q.getNext())
+			if (reachable.contains(q) && canReachEnd.contains(q))
 			{
-				if (!seen.contains(next))
-				{
-					dfs(next, seen);
-				}
+				this.liveStates.add(q);
 			}
 		}
-		
-		private void reverseDfs(FAState q, Set<FAState> seen)
+
+
+	}
+
+	/**
+	 * Query whether a state is live or not. Must be called after trim() has been run.
+	 * @param q The state to query
+	 * @return true if it is reachable, false otherwise
+	 */
+	public boolean isLive(FAState q)
+	{
+		return this.liveStates.contains(q);
+	}
+
+	/**
+	 * Find reachable states using recursive DFS
+	 * @param q The state to start DFS at
+	 * @param seen The set of all states already reached by DFS
+	 */
+	private void dfs(FAState q, Set<FAState> seen)
+	{
+		seen.add(q);
+		for (FAState next : q.getNext())
 		{
-			seen.add(q);
-			for (FAState prev : q.getPre())
+			if (!seen.contains(next))
 			{
-				if (!seen.contains(prev))
-				{
-					reverseDfs(prev, seen);
-				}
+				dfs(next, seen);
 			}
 		}
-		
-		public  int minWordLength()
+	}
+
+	/**
+	 * Perform a backwards DFS, seeing which states can reach some final state
+	 * @param q The state to start at
+	 * @param seen the states already seen
+	 */
+	private void reverseDfs(FAState q, Set<FAState> seen)
+	{
+		seen.add(q);
+		for (FAState prev : q.getPre())
 		{
-			this.trim();
-			return -1;
+			if (!seen.contains(prev))
+			{
+				reverseDfs(prev, seen);
+			}
 		}
-		
-		
+	}
+
+
+
+
 }
